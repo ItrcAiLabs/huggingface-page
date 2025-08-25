ae0a5da48066fbee54fed559b894c84d2c6c4e4e
AIlabs <ailabs-itrc@users.noreply.huggingface.co>
2025-08-25 11:26:53 +0000

    Update test_assets.py

--- test_assets.py
+++ test_assets.py
@@ -5,14 +5,14 @@ with gr.Blocks() as demo:
 
     gr.HTML(
         '<div style="display:flex; gap:20px; flex-wrap:wrap;">'
-        '<div><img src="file=static/brands/openai.svg" width="48"><br>OpenAI</div>'
-        '<div><img src="file=static/brands/anthropic.svg" width="48"><br>Anthropic</div>'
-        '<div><img src="file=static/brands/google.svg" width="48"><br>Google</div>'
-        '<div><img src="file=static/brands/meta.svg" width="48"><br>Meta</div>'
-        '<div><img src="file=static/brands/qwen.webp" width="48"><br>Qwen</div>'
-        '<div><img src="file=static/brands/mistral.svg" width="48"><br>Mistral</div>'
-        '<div><img src="file=static/brands/deepseek.svg" width="48"><br>DeepSeek</div>'
-        '<div><img src="file=static/brands/xai.svg" width="48"><br>xAI</div>'
+        '<div><img src="static/brands/openai.svg" width="48"><br>OpenAI</div>'
+        '<div><img src="static/brands/anthropic.svg" width="48"><br>Anthropic</div>'
+        '<div><img src="static/brands/google.svg" width="48"><br>Google</div>'
+        '<div><img src="static/brands/meta.svg" width="48"><br>Meta</div>'
+        '<div><img src="static/brands/qwen.webp" width="48"><br>Qwen</div>'
+        '<div><img src="static/brands/mistral.svg" width="48"><br>Mistral</div>'
+        '<div><img src="static/brands/deepseek.svg" width="48"><br>DeepSeek</div>'
+        '<div><img src="static/brands/xai.svg" width="48"><br>xAI</div>'
         '</div>'
     )
 
